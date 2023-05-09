9025e8351e40b7abd15319cf5f56e4c258636e45
boukhalfa khaled islam <105537974+islame75@users.noreply.github.com>
2023-05-09 15:01:07 -0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bubbleBtn = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Blue;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.bubbleBtn);
+            this.panel1.Controls.Add(this.btnReset);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1007, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(689, 55);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(55, 38);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "normal";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Blue;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(689, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "slow";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(774, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number Of Elements";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(880, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 37);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "100";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(820, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "50";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(760, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "10";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bubbleBtn
+            // 
+            this.bubbleBtn.BackColor = System.Drawing.Color.Red;
+            this.bubbleBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bubbleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bubbleBtn.ForeColor = System.Drawing.Color.White;
+            this.bubbleBtn.Location = new System.Drawing.Point(302, 12);
+            this.bubbleBtn.Name = "bubbleBtn";
+            this.bubbleBtn.Size = new System.Drawing.Size(149, 38);
+            this.bubbleBtn.TabIndex = 0;
+            this.bubbleBtn.Text = "Bubble Sort";
+            this.bubbleBtn.UseVisualStyleBackColor = false;
+            this.bubbleBtn.Click += new System.EventHandler(this.BtnBubble_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(12, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(248, 73);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "init";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(2, 99);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1000, 500);
+            this.panel2.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Blue;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(628, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(55, 38);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "very slow";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(940, 48);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(54, 37);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "500";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 603);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1023, 642);
+            this.MinimumSize = new System.Drawing.Size(1023, 642);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button bubbleBtn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int[] TheArray;
+        Graphics g;
+        int width = 1000;
+        int elArray = 50;
+        int delay = 0;
+        int maxVal = 500;
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            g = panel2.CreateGraphics();
+            
+            
+
+            int xDraw = width / elArray;
+                
+            TheArray = new int[elArray];
+            g.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.Black), 0, 0, width, maxVal);
+            Random rand = new Random();
+            for (int i = 0; i < elArray; i++)
+            {
+                TheArray[i] = rand.Next(0, maxVal);
+            }
+            for (int i = 0; i < elArray; i++)
+            {
+                g.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.Wheat), i * xDraw, maxVal - TheArray[i], xDraw-1, maxVal);
+
+
+            }
+        }
+
+        private void BtnBubble_Click(object sender, EventArgs e)
+        {
+            BubbleSort b = new BubbleSort();
+            Thread sort = new Thread(() => b.DoWork(TheArray, g, maxVal, elArray, width, delay));
+            //b.DoWork(TheArray, g, maxVal, elArray, width, delay);
+            sort.Start();
+           
+            
+
+        }
+      public class BubbleSort
+        {
+            private bool _sorted = false;
+            private int[] theArray;
+            private Graphics g;
+            private int maxVal;
+            private int elArray;
+            private int numEntries;
+            private int xDraw ;
+            private int delay;
+            Brush whiteBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Wheat);
+            Brush blackBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+            Brush GreenBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Green);
+            Brush YellowBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Yellow);
+            
+
+            public void DoWork(int[] TheArray_i, Graphics g_i, int maxVal_i,int elArray_i, int numEntries_i, int delay_i)
+                {
+                
+                    theArray = TheArray_i;
+                    g = g_i;
+                    maxVal = maxVal_i;
+                    elArray = elArray_i;
+                    numEntries = numEntries_i;
+                   xDraw = numEntries / elArray;
+                    this.delay = delay_i;
+                    while (!_sorted)
+                    {
+                    int jk = theArray.Count(); 
+                        for (int i = 0; i < jk-1; i++)
+                        {
+                            if (theArray[i] > theArray[i + 1])
+                            {
+                                Swap(i);
+                           // Thread.Sleep(delay);
+                            }
+                        }
+                    jk--;
+                        _sorted = isSorted();
+                    }
+                }
+
+            private void Swap(int i)
+            {
+                g.FillRectangle(blackBrush, i * xDraw, 0, xDraw - 1, maxVal);
+                g.FillRectangle(blackBrush, (i + 1) * xDraw, 0, xDraw - 1, maxVal);
+                //  g.FillRectangle(blackBrush, i, 0, 2, maxVal);
+
+                g.FillRectangle(YellowBrush, i * xDraw, maxVal - theArray[i], xDraw - 1, maxVal);
+                g.FillRectangle(GreenBrush, (i + 1) * xDraw, maxVal - theArray[i + 1], xDraw - 1, maxVal);
+                Thread.Sleep(delay);
+              //  int temp = theArray[i];
+               // theArray[i] = theArray[i + 1];
+               // theArray[i + 1] = temp;
+
+                theArray[i] = theArray[i] + theArray[i + 1];
+                theArray[i + 1] = theArray[i] - theArray[i + 1];
+                theArray[i] = theArray[i] - theArray[i + 1];
+
+                    
+
+                g.FillRectangle(blackBrush, i * xDraw, 0, xDraw - 1, maxVal);
+                g.FillRectangle(blackBrush, (i + 1) * xDraw, 0, xDraw - 1, maxVal);
+                //  g.FillRectangle(blackBrush, i, 0, 2, maxVal);
+                g.FillRectangle(YellowBrush, i * xDraw, maxVal - theArray[i], xDraw - 1, maxVal);
+                g.FillRectangle(GreenBrush, (i + 1) * xDraw, maxVal - theArray[i + 1], xDraw - 1, maxVal);
+                Thread.Sleep(delay);
+
+                g.FillRectangle(blackBrush, i* xDraw, 0, xDraw- 1, maxVal);
+                    g.FillRectangle(blackBrush, (i + 1)* xDraw, 0, xDraw - 1, maxVal);
+                    //g.FillRectangle(blackBrush, i, 0, 2, maxVal);
+
+                    g.FillRectangle(whiteBrush, i * xDraw, maxVal - theArray[i], xDraw - 1, maxVal);
+                    g.FillRectangle(whiteBrush, (i + 1) * xDraw, maxVal - theArray[i + 1], xDraw - 1, maxVal);
+                }
+
+                private bool isSorted()
+                {
+                    for (int i = 0; i < theArray.Count() - 1; i++)
+                    {
+                        if (theArray[i] > theArray[i + 1]) return false;
+                    }
+               
+                    return true;
+                }
+            }
+
+      
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            elArray = 100;
+            button3.BackColor = Color.Red;
+            button2.BackColor = Color.Blue;
+            button1.BackColor = Color.Blue;
+            button7.BackColor = Color.Blue;
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            elArray = 50;
+            button2.BackColor = Color.Red;
+            button3.BackColor = Color.Blue;
+            button1.BackColor = Color.Blue;
+            button7.BackColor = Color.Blue;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            elArray = 10;
+            button1.BackColor = Color.Red;
+            button2.BackColor = Color.Blue;
+            button3.BackColor = Color.Blue;
+            button7.BackColor = Color.Blue;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            delay = 30;
+            button4.BackColor = Color.Red;
+            button5.BackColor = Color.Blue;
+            button6.BackColor = Color.Blue;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            delay = 0;
+            button5.BackColor = Color.Red;
+            button4.BackColor = Color.Blue;
+            button6.BackColor = Color.Blue;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            delay = 500;
+            button6.BackColor = Color.Red;
+            button4.BackColor = Color.Blue;
+            button5.BackColor = Color.Blue;
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            elArray = 500;
+            button7.BackColor = Color.Red ;
+            button2.BackColor = Color.Blue;
+            button1.BackColor = Color.Blue;
+            button3.BackColor = Color.Blue;
+        }
+    }
+    }
